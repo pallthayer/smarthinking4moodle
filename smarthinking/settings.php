5e5297b644d799e5d62fc5244c56516c9d2e2747
Pall Thayer <pallthay@gmail.com>
2010-02-16 12:20:46 -0500

    Fixed some text in the admin settings page.

--- smarthinking/settings.php
+++ smarthinking/settings.php
@@ -1,9 +1,9 @@
 <?php
 
-$settings->add(new admin_setting_configtext('block_smarthinking_partnerid', "Partner ID", get_string('clientpartnerid', 'block_smarthinking'), '', PARAM_TEXT)); 
+$settings->add(new admin_setting_configtext('block_smarthinking_partnerid', "Partner ID", 'clientpartnerid', '', PARAM_TEXT)); 
 
-$settings->add(new admin_setting_configtext('block_smarthinking_partnerpass', "Partner password", get_string('clientpartnerpass', 'block_smarthinking'), '', PARAM_TEXT));
+$settings->add(new admin_setting_configtext('block_smarthinking_partnerpass', "Partner password", 'clientpartnerpass', '', PARAM_TEXT));
 
-$settings->add(new admin_setting_configtext('block_smarthinking_partnerzip', "Zipcode", get_string('clientpartnerzip', 'block_smarthinking'), '', PARAM_INT));
+$settings->add(new admin_setting_configtext('block_smarthinking_partnerzip', "Zipcode", 'clientpartnerzip', '', PARAM_INT));
 
 ?>
